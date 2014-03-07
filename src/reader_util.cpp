44110c3c3e61f5fad24f6d712630e965dea5ef15
Francisco de la Peña <fran@fran.cr>
2014-03-06 19:39:54 -0600

    Fix japanese locale name

--- src/reader_util.cpp
+++ src/reader_util.cpp
@@ -50,7 +50,7 @@ std::string ReaderUtil::GetEncoding(const std::string& ini_file) {
 		std::string loc_lang = loc.name().substr(0, loc.name().find_first_of("_"));
 
 		if      (loc_lang == "th")    default_enc = "874";
-		else if (loc_lang == "jp")    default_enc = "932";
+		else if (loc_lang == "ja")    default_enc = "932";
 		else if (loc_full == "zh_CN" ||
 		         loc_full == "zh_SG") default_enc = "936";
 		else if (loc_lang == "ko")    default_enc = "949";
