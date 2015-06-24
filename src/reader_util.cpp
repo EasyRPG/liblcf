b331a2a44617f7ee2b93fad576ae7754f215e4d1
Francisco de la Peña <fran@fran.cr>
2015-06-23 22:12:41 -0600

    Print ICU encoding conversion error names instead of code numbers and more verbose

--- src/reader_util.cpp
+++ src/reader_util.cpp
@@ -267,7 +267,7 @@ std::string ReaderUtil::Recode(const std::string& str_to_encode,
 	conv = ucnv_open(src_enc_str.c_str(), &status);
 	
 	if (status != U_ZERO_ERROR && status != U_AMBIGUOUS_ALIAS_WARNING) {
-		fprintf(stderr, "liblcf: ucnv_open(\"%s\") source encoding error: %d\n", src_enc_str.c_str(), status);
+		fprintf(stderr, "liblcf:  ucnv_open() error for source encoding \"%s\": %s\n", src_enc_str.c_str(), u_errorName(status));
 		return std::string();
 	}
 	status = U_ZERO_ERROR;
@@ -275,7 +275,7 @@ std::string ReaderUtil::Recode(const std::string& str_to_encode,
 	length = ucnv_toUChars(conv, unicode_str, size, str_to_encode.c_str(), -1, &status);
 	ucnv_close(conv);
 	if (status != U_ZERO_ERROR) {
-		fprintf(stderr, "liblcf: ucnv_toUChars() error: %d\n", status);
+		fprintf(stderr, "liblcf: ucnv_toUChars() error when encoding \"%s\": %s\n", str_to_encode.c_str(), u_errorName(status));
 		return std::string();
 	}
 
@@ -283,7 +283,7 @@ std::string ReaderUtil::Recode(const std::string& str_to_encode,
 
 	conv = ucnv_open(dst_enc_str.c_str(), &status);
 	if (status != U_ZERO_ERROR && status != U_AMBIGUOUS_ALIAS_WARNING) {
-		fprintf(stderr, "liblcf: ucnv_open(\"%s\") destination encoding error: %d\n", dst_enc_str.c_str(), status);
+		fprintf(stderr, "liblcf: ucnv_open() error for destination encoding \"%s\": %s\n", dst_enc_str.c_str(), u_errorName(status));
 		return std::string();
 	}
 	status = U_ZERO_ERROR;
@@ -291,7 +291,7 @@ std::string ReaderUtil::Recode(const std::string& str_to_encode,
 	ucnv_fromUChars(conv, result, length * 4, unicode_str, -1, &status);
 	ucnv_close(conv);
 	if (status != U_ZERO_ERROR) {
-		fprintf(stderr, "liblcf: ucnv_fromUChars() error: %d\n", status);
+		fprintf(stderr, "liblcf: ucnv_fromUChars() error: %s\n", u_errorName(status));
 		return std::string();
 	}
 
