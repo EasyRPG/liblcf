11673353a124b753a05fa8cbe3eeaa70d144ab4d
Carsten Teibes <dev@f4ke.de>
2017-03-23 21:23:59 +0100

    Update copyright headers (year, wording)

--- tests/time_stamp.cpp
+++ tests/time_stamp.cpp
@@ -1,3 +1,12 @@
+/*
+ * This file is part of liblcf. Copyright (c) 2017 liblcf authors.
+ * https://github.com/EasyRPG/liblcf - https://easyrpg.org
+ *
+ * liblcf is Free/Libre Open Source Software, released under the MIT License.
+ * For the full copyright and license information, please view the COPYING
+ * file that was distributed with this source code.
+ */
+
 #include <cassert>
 #include <cmath>
 #include <cstdlib>
