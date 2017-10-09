68b5b42c9669b30b2b9723526a2ac6102e56d5cf
Carsten Teibes <dev@f4ke.de>
2017-10-09 04:44:10 +0200

    Rewrite timestamp tests using doctest Add doctest, migrate Autotools and CMake build to it Adapt .gitignore

--- tests/time_stamp.cpp
+++ tests/time_stamp.cpp
@@ -7,52 +7,51 @@
  * file that was distributed with this source code.
  */
 
-#include <cassert>
-#include <cmath>
-#include <cstdlib>
-#include <iostream>
 #include "lsd_reader.h"
+#include "doctest.h"
 
+TEST_SUITE_BEGIN("timestamps");
 
-static void ToTDateTime() {
-#define CheckTime(val, year, month, day, hour, minute, second) { \
-			std::time_t const current = LSD_Reader::ToUnixTimestamp(val);			 \
-		struct tm const* const t = std::gmtime(&current); \
-		 \
-		assert(t->tm_year == year - 1900); \
-		assert(t->tm_mon + 1 == month); \
-		assert(t->tm_mday == day); \
-		assert(t->tm_hour == hour); \
-		assert(t->tm_min == minute); \
-		assert(t->tm_sec == second); \
-	} \
+static void CheckTime(double val, int year, int month, int day, int hour,
+                      int minute, int second) {
+	std::time_t const current = LSD_Reader::ToUnixTimestamp(val);
+	std::tm const* t = std::gmtime(&current);
 
-	// 27468.96875   27468     1975 年 3 月 15 日    .96875    11:15:00 P.M.
+	CHECK_EQ(t->tm_year, year - 1900);
+	CHECK_EQ(t->tm_mon + 1, month);
+	CHECK_EQ(t->tm_mday, day);
+	CHECK_EQ(t->tm_hour, hour);
+	CHECK_EQ(t->tm_min, minute);
+	CHECK_EQ(t->tm_sec, second);
+}
+
+TEST_CASE("comparison") {
+	// 27468.96875 => 1975 年 3 月 15 日 11:15:00 P.M.
 	CheckTime(27468.96875, 1975, 3, 15, 23, 15, 0);
-	// 36836.125     36836     2000 年 11 月 6 日    .125       3:00:00 A.M.
+
+	// 36836.125   => 2000 年 11 月 6 日  3:00:00 A.M.
 	CheckTime(36836.125, 2000, 11, 6, 3, 0, 0);
-	// 36295.0                 1999/5/15           1999/05/15 12:00:00 AM
+
+	// 36295.0     => 1999/5/15         12:00:00 AM
 	CheckTime(36295.0, 1999, 5, 15, 0, 0, 0);
-	// 36232.9375              1999/03/13 22:30:00 1999/03/13 10:30:00 PM
+
+	// 36232.9375  => 1999/3/13         22:30:00 (10:30:00 PM)
 	CheckTime(36232.9375, 1999, 3, 13, 22, 30, 0);
-
-#undef CheckTime
 }
 
-static void ToUnixTimestamp() {
+TEST_CASE("conversion to Unix timestamp") {
 	std::time_t const current = std::time(NULL);
-	assert(current == LSD_Reader::ToUnixTimestamp(LSD_Reader::ToTDateTime(current)));
+	double const current_tdatetime = LSD_Reader::ToTDateTime(current);
+
+	CHECK(current == LSD_Reader::ToUnixTimestamp(current_tdatetime));
 }
 
-static void GenerateTimestamp() {
+TEST_CASE("conversion to TDateTime") {
 	double const current = LSD_Reader::ToTDateTime(std::time(NULL));
-	assert(current == LSD_Reader::ToTDateTime(LSD_Reader::ToUnixTimestamp(current)));
+	std::time_t const current_timestamp = LSD_Reader::ToUnixTimestamp(current);
+
+	/* small rounding difference allowed */
+	CHECK(doctest::Approx(current) == LSD_Reader::ToTDateTime(current_timestamp));
 }
 
-int main() {
-  ToTDateTime();
-  ToUnixTimestamp();
-  GenerateTimestamp();
-
-  return EXIT_SUCCESS;
-}
+TEST_SUITE_END();
