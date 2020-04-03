83a434ce0503e0f29016b584993848ba818bd7bd
Matthew Fioravante <fmatthew5876@gmail.com>
2020-04-03 11:42:05 -0400

    C++14 fixes

--- src/rpg_fixup.cpp
+++ src/rpg_fixup.cpp
@@ -147,76 +147,70 @@ void RPG::SaveSystem::Fixup() {
 void RPG::SaveSystem::UnFixup() {
 	const RPG::System& system = lcf::Data::system;
 
-	//TODO: Should be a C++14 polymorphic lambda
-	auto reset_se = [](RPG::Sound& s) {
-		s = {};
-		s.name.clear();
-	};
-
-	auto reset_bgm = [](RPG::Music& s) {
+	auto reset_audio = [](auto& s) {
 		s = {};
 		s.name.clear();
 	};
 
 	if (battle_music == system.battle_music) {
-		reset_bgm(battle_music);
+		reset_audio(battle_music);
 	}
 	if (battle_end_music == system.battle_end_music) {
-		reset_bgm(battle_end_music);
+		reset_audio(battle_end_music);
 	}
 	if (inn_music == system.inn_music) {
-		reset_bgm(inn_music);
+		reset_audio(inn_music);
 	}
 	if (title_music == system.title_music) {
-		reset_bgm(title_music);
+		reset_audio(title_music);
 	}
 	if (boat_music == system.boat_music) {
-		reset_bgm(boat_music);
+		reset_audio(boat_music);
 	}
 	if (ship_music == system.ship_music) {
-		reset_bgm(ship_music);
+		reset_audio(ship_music);
 	}
 	if (airship_music == system.airship_music) {
-		reset_bgm(airship_music);
+		reset_audio(airship_music);
 	}
 	if (gameover_music == system.gameover_music) {
-		reset_bgm(gameover_music);
+		reset_audio(gameover_music);
 	}
 	if (cursor_se == system.cursor_se) {
-		reset_se(cursor_se);
+		reset_audio(cursor_se);
 	}
 	if (decision_se == system.decision_se) {
-		reset_se(decision_se);
+		reset_audio(decision_se);
 	}
 	if (cancel_se == system.cancel_se) {
-		reset_se(cancel_se);
+		reset_audio(cancel_se);
 	}
 	if (buzzer_se == system.buzzer_se) {
-		reset_se(buzzer_se);
+		reset_audio(buzzer_se);
 	}
 	if (battle_se == system.battle_se) {
-		reset_se(battle_se);
+		reset_audio(battle_se);
 	}
 	if (escape_se == system.escape_se) {
-		reset_se(escape_se);
+		reset_audio(escape_se);
 	}
 	if (enemy_attack_se == system.enemy_attack_se) {
-		reset_se(enemy_attack_se);
+		reset_audio(enemy_attack_se);
 	}
 	if (enemy_damaged_se == system.enemy_damaged_se) {
-		reset_se(enemy_damaged_se);
+		reset_audio(enemy_damaged_se);
 	}
 	if (actor_damaged_se == system.actor_damaged_se) {
-		reset_se(actor_damaged_se);
+		reset_audio(actor_damaged_se);
 	}
 	if (dodge_se == system.dodge_se) {
-		reset_se(dodge_se);
+		reset_audio(dodge_se);
 	}
 	if (enemy_death_se == system.enemy_death_se) {
-		reset_se(enemy_death_se);
+		reset_audio(enemy_death_se);
 	}
 	if (item_se == system.item_se) {
-		reset_se(item_se);
+		reset_audio(item_se);
 	}
 }
 
