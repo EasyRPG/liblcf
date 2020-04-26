ce2f51d3187e4b0c7f496850afe1c04b96d3d157
Carsten Teibes <dev@f4ke.de>
2020-04-26 11:15:26 +0200

    Update copyright years

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -1,3 +1,12 @@
+/*
+ * This file is part of liblcf. Copyright (c) 2020 liblcf authors.
+ * https://github.com/EasyRPG/liblcf - https://easyrpg.org
+ *
+ * liblcf is Free/Libre Open Source Software, released under the MIT License.
+ * For the full copyright and license information, please view the COPYING
+ * file that was distributed with this source code.
+ */
+
 #include "lcf_options.h"
 #include "encoder.h"
 #include "reader_util.h"
