8ef1e8bf12b98c06cb121b46693487b76d97aaae
Matthew Fioravante <fmatthew5876@gmail.com>
2020-08-16 05:07:55 -0400

    Skill - use DBString

--- src/generated/ldb_skill.cpp
+++ src/generated/ldb_skill.cpp
@@ -20,28 +20,28 @@ namespace lcf {
 
 template <>
 char const* const Struct<rpg::Skill>::name = "Skill";
-static TypedField<rpg::Skill, std::string> static_name(
+static TypedField<rpg::Skill, DBString> static_name(
 	&rpg::Skill::name,
 	LDB_Reader::ChunkSkill::name,
 	"name",
 	0,
 	0
 );
-static TypedField<rpg::Skill, std::string> static_description(
+static TypedField<rpg::Skill, DBString> static_description(
 	&rpg::Skill::description,
 	LDB_Reader::ChunkSkill::description,
 	"description",
 	0,
 	0
 );
-static TypedField<rpg::Skill, std::string> static_using_message1(
+static TypedField<rpg::Skill, DBString> static_using_message1(
 	&rpg::Skill::using_message1,
 	LDB_Reader::ChunkSkill::using_message1,
 	"using_message1",
 	0,
 	0
 );
-static TypedField<rpg::Skill, std::string> static_using_message2(
+static TypedField<rpg::Skill, DBString> static_using_message2(
 	&rpg::Skill::using_message2,
 	LDB_Reader::ChunkSkill::using_message2,
 	"using_message2",
