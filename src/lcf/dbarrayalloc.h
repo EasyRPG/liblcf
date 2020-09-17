84949d7ab2e3df0c7610c72df40543ff3b8defeb
Ghabry <gabriel+github@mastergk.de>
2020-09-17 18:53:53 +0200

    make _empty_buf const instead of constexpr
    C++17 made constexpr implicity inline which gives linker errors here because lcf is compiled as C++14 but Editor is C++17

--- src/lcf/dbarrayalloc.h
+++ src/lcf/dbarrayalloc.h
@@ -26,7 +26,7 @@ struct DBArrayAlloc {
 	static void* alloc(size_type size, size_type field_size, size_type align);
 	static void free(void* p, size_type align) noexcept;
 
-	static constexpr void* empty_buf() {
+	static void* empty_buf() {
 		return const_cast<size_type*>(&_empty_buf[1]);
 	}
 
@@ -39,7 +39,7 @@ struct DBArrayAlloc {
 	}
 
 	private:
-	static constexpr const size_type _empty_buf[2] = { 0, 0 };
+	static const size_type _empty_buf[2];
 };
 
 } // namespace lcf
