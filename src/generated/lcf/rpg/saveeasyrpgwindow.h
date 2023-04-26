3c465c0b81536465cd53dd2c20b677f450152d60
Ghabry <gabriel+github@mastergk.de>
2023-04-26 21:08:45 +0200

    Change version to 0.8

--- src/generated/lcf/rpg/saveeasyrpgwindow.h
+++ src/generated/lcf/rpg/saveeasyrpgwindow.h
@@ -1,7 +1,7 @@
 /* !!!! GENERATED FILE - DO NOT EDIT !!!!
  * --------------------------------------
  *
- * This file is part of liblcf. Copyright (c) 2021 liblcf authors.
+ * This file is part of liblcf. Copyright (c) liblcf authors.
  * https://github.com/EasyRPG/liblcf - https://easyrpg.org
  *
  * liblcf is Free/Libre Open Source Software, released under the MIT License.
