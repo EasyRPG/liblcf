b840c7acff9c0c8066002f12da9a7f1da9d3dcfb
Ghabry <gabriel+github@mastergk.de>
2023-04-26 17:55:49 +0200

    Release helper: remove updating of years from source files

--- builds/release-helper.sh
+++ builds/release-helper.sh
@@ -38,23 +38,6 @@ fi
 echo "Updating License…"
 sed -i "1,1 s/2014-2[0-9][0-9][0-9]/2014-$year/" COPYING
 
-# update copyright years, but filter out external sources
-echo "Updating source files…"
-
-find src tests -maxdepth 3 \
-  \( -path src/generated -o -path src/lcf/third_party \) -prune \
-  -o -type f -a \( -name "*.h" -o -name "*.cpp" \) \
-    -a \! \( -name "ini*" -o -name "doctest*" \) \
-  -exec sed -i "/liblcf\. Copyright/,1 s/2[0-9][0-9][0-9]/$year/" {} +
-
-find tools -maxdepth 1 -type f -a \( -name "*.h" -o -name "*.cpp" \) \
-  -exec sed -i "/Copyright/,1 s/2[0-9][0-9][0-9]/$year/" {} +
-
-# updating header for generated source files
-grep -q "liblcf\. Copyright.*$year" generator/templates/copyright.tmpl ||
-  echo " -> You need to run the generator."
-sed -i "/liblcf\. Copyright/,1 s/2[0-9][0-9][0-9]/$year/" generator/templates/copyright.tmpl
-
 cat << EOF
 
 If everything is ready and committed, use these commands to publish the git tag:
